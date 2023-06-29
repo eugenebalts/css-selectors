4ed1a762b5a82149f3aa7610b055b6fa068dfbb1
eugenebalts <norcobeatz@gmail.com>
2023-06-29 11:21:05 +0200

    feat: do nav for levels in level-controls module

---
+++ rs-selectors/src/components/view/level-controls/level-controls.ts
@@ -0,0 +1,25 @@
+import './level-controls.css';
+import LEVELS from '../../controller/manageLevels//levels';
+
+export default class LevelControl {
+    sidebar: HTMLDivElement | null = document.querySelector('.sidebar');
+
+    writeLevels(lvl: number) {
+        const levelsWrapper: HTMLDivElement = document.createElement('div');
+        levelsWrapper.classList.add('levels-control');
+        for (const level of LEVELS) {
+            const levelTitle: HTMLParagraphElement = document.createElement('p');
+            levelTitle.classList.add('levels__title',  `levels__title_${level.level}`);
+            levelTitle.innerHTML = `<span>Level ${level.level}</span> ${level.name}`;
+            
+            if (lvl) {
+                if (lvl === level.level) levelTitle.classList.add('levels__title_active');
+            } else if (level.level === 1) {
+                levelTitle.classList.add('levels__title_active');
+            }
+
+            levelsWrapper.append(levelTitle);
+        }
+        this.sidebar?.append(levelsWrapper);
+    }
+}
