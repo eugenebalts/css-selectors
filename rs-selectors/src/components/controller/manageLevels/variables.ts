e19083d534f1a2d7030f023655befb8466a67d11
eugenebalts <norcobeatz@gmail.com>
2023-07-01 12:42:33 +0200

    feat & refactor: add list of passed Levels & change property

--- rs-selectors/src/components/controller/manageLevels/variables.ts
+++ rs-selectors/src/components/controller/manageLevels/variables.ts
@@ -1,11 +1,16 @@
+const lsGetPassed = localStorage.getItem('passed');
+const lsGetCurrent = localStorage.getItem('currentLevel');
+
 const variables: IVars = {
-	currentLevel: localStorage.getItem('level') || 1,
+	currentLevel: lsGetCurrent ? JSON.parse(lsGetCurrent) : 1,
 	maxLevel: 4,
+	passedLevels: lsGetPassed ? [...JSON.parse(lsGetPassed)] : [],
 };
 
 interface IVars {
-	currentLevel: string | number,
+	currentLevel: number,
 	maxLevel: number,
+	passedLevels: number[];
 }
 
 export default variables;
