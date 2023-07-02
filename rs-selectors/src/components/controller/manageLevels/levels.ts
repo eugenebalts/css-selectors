3546ccf724dbcc1d4bf9b6a2ffd46e8b7582b934
eugenebalts <norcobeatz@gmail.com>
2023-07-02 15:40:13 +0200

    feat: add levels

--- rs-selectors/src/components/controller/manageLevels/levels.ts
+++ rs-selectors/src/components/controller/manageLevels/levels.ts
@@ -81,7 +81,7 @@ const LEVELS: Levels = [
 				classGeneral: 'vehicle'
 			},
 			{
-				image: 'car-truck1',
+				image: 'car-truck2',
 				row: 'vehicle_down-line',
 				classGeneral: 'vehicle'
 			},
@@ -142,30 +142,161 @@ const LEVELS: Levels = [
 	},
 	{
 		level: 7,
-		name: 'A#id, #id B',
-		class: 'level-6',
+		name: '#id ~ A',
+		class: 'level-7',
 		objects: [
 			{
 				image: 'car-police1',
 				row: 'vehicle_top-line',
 				classGeneral: 'vehicle',
-				id: 'police',
 			},
 			{
-				image: 'car-green1',
-				row: 'vehicle_top-line',
+				image: 'car-truck2',
+				row: 'vehicle_down-line',
 				classGeneral: 'vehicle',
-				classAdditional: 'green'
 			},
 			{
 				image: 'car-red2',
 				row: 'vehicle_top-line',
 				classGeneral: 'vehicle',
-				classAdditional: 'red'
 			},
 		],
-		objectsToFind: ['car-police1', 'car-green1'],	
-		rightAnswer: '#police, .green',
+		objectsToFind: ['car-truck2'],	
+		rightAnswer: '#police ~ truck',
+	},
+	{
+		level: 8,
+		name: 'nth-child(n)',
+		class: 'level-8',
+		objects: [
+			{
+				image: 'car-red1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck1',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck2',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+		],
+		objectsToFind: ['car-truck2'],	
+		rightAnswer: 'truck:nth-child(2)',
+	},
+	{
+		level: 9,
+		name: ':empty',
+		class: 'level-9',
+		objects: [
+			{
+				image: 'car-red1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck1',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck2',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			
+		],
+		objectsToFind: ['car-truck2'],	
+		rightAnswer: 'truck:empty',
+	},
+	{
+		level: 10,
+		name: '[attribute]',
+		class: 'level-10',
+		objects: [
+			{
+				image: 'car-red1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-police1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck2',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			
+		],
+		objectsToFind: ['car-red1'],	
+		rightAnswer: '[private]',
+	},
+	{
+		level: 11,
+		name: '[attribute="value"]',
+		class: 'level-11',
+		objects: [
+			{
+				image: 'car-red1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-police1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck2',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			
+		],
+		objectsToFind: ['car-police1', 'car-truck2'],	
+		rightAnswer: '[owner="Government"]',
+	},
+	{
+		level: 12,
+		name: '*',
+		class: 'level-12',
+		objects: [
+			{
+				image: 'car-red1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-police1',
+				row: 'vehicle_top-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-truck2',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'car-red2',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			{
+				image: 'scooter1',
+				row: 'vehicle_down-line',
+				classGeneral: 'vehicle',
+			},
+			
+		],
+		objectsToFind: ['car-red1', 'car-police1', 'car-truck2', 'scooter1', 'car-red2'],	
+		rightAnswer: '*',
 	},
 ];
 
