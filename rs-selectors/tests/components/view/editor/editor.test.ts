72ec9269c7dd5659eb378d13cf225198ede721ed
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-10 00:15:55 +0200

    feat: write test for check how inputCss cleaning after helpBtn is pushed

--- rs-selectors/tests/components/view/editor/editor.test.ts
+++ rs-selectors/tests/components/view/editor/editor.test.ts
@@ -1,9 +1,22 @@
 import Editor from "../../../../src/components/view/editor/editor";
+import variables from "../../../../src/components/controller/manageLevels/variables";
 
 describe('Editor', () => {
-    const editor = new Editor();
+    let editor = new Editor();
+    let mockVariables,
+        mockInputCss,
+        mockHelpBtn
+    ;
+    document.body.innerHTML = `
+        <div class="field"></div>
+        <input class="input__input-css" type="text">
+        <button class="editor__help-btn"></button>
+    `;
+
+    
 
     beforeEach(() => {
+        editor = new Editor();
         localStorage.clear();
     });
 
@@ -15,5 +28,16 @@ describe('Editor', () => {
         expect(passedLevels).toBe("[6]");
         expect(newCurrentLevel).toBe('7');
     });
-    
+
+    test("Should clean inputCss and make button disabled", () => {
+        const inputCss = document.querySelector('.input__input-css') as HTMLInputElement;
+        const helpBtn = document.querySelector('.editor__help-btn') as HTMLButtonElement;
+
+        inputCss.value = 'Our old solution';
+        editor.help();
+
+        expect(inputCss.value).toBe('');
+        expect(helpBtn.disabled).toBe(true);
+    });
+
 })
