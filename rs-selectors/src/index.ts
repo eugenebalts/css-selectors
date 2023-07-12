1d459a0fc5a2d66617743bd94dd78ed62e2de7a5
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-12 12:14:47 +0200

    refactor: remove function

--- rs-selectors/src/index.ts
+++ rs-selectors/src/index.ts
@@ -1,9 +1,6 @@
 import './global.css';
 import App from './components/controller/app';
 
-function AppInit(): void {
-	const Application = new App();
-	Application.start();
-}
+const Application = new App();
+Application.start();
 
-AppInit();
