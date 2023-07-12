01f41d3eedfb30bc0ae312cd47d23bcb6d50f7ed
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-12 12:15:06 +0200

    refactor: keep DRY principle

--- rs-selectors/src/components/controller/manageLevels/variables.ts
+++ rs-selectors/src/components/controller/manageLevels/variables.ts
@@ -1,15 +1,16 @@
 import { IVars } from '../../../types';
 
-const lsGetPassed: string | null = localStorage.getItem('passed');
-const lsGetCurrent: string | null = localStorage.getItem('currentLevel');
-const lsGetHinted: string | null = localStorage.getItem('hinted');
+function getFromLocalStorage <T extends string> (item: T) {
+	const gotValue = localStorage.getItem(item);
+	if (gotValue) return JSON.parse(gotValue);
+}
 
 const variables: IVars = {
-	currentLevel: lsGetCurrent ? JSON.parse(lsGetCurrent) : 1,
+	currentLevel: getFromLocalStorage('currentLevel') || 1,
 	maxLevel: 13,
-	passedLevels: lsGetPassed ? [...JSON.parse(lsGetPassed)] : [],
+	passedLevels: getFromLocalStorage('passed') || [],
 	isHintUsed: false,
-	passedWithHint: lsGetHinted ? [...JSON.parse(lsGetHinted)] : [],
+	passedWithHint: getFromLocalStorage('hinted') || [],
 };
 
 export default variables;
